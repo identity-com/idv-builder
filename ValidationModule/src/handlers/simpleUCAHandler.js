2c0e0b9ca88a297b4fe96a5f3b61951a37f32b7e
dankelleher <dankelleher@yahoo.com>
2020-07-02 19:39:03 +0200

    CIV-53 Some simple handlers to demonstrate the handler base classes in idv-commons

--- ValidationModule/src/handlers/simpleUCAHandler.js
+++ ValidationModule/src/handlers/simpleUCAHandler.js
@@ -1,16 +1,33 @@
 /* eslint-disable-file no-param-reassign,class-methods-use-this */
-const { UCAHandler, Context } = require('@identity.com/idv-commons');
+const { UCAHandler, Context, Constants: { UCAStatus} } = require('@identity.com/idv-commons');
 
 // a handler that accepts all UCA values
-class SimpleUCAHandler extends UCAHandler {
-  async handleUCA(value, ucaState) {
-    Context.log('SimpleUCAHandler accepted uca value', { value, ucaState })
+class AutoPassUCAHandler extends UCAHandler {
+  constructor(ucaName) {
+    super(ucaName, true);
   }
 }
 
-module.exports = {
-  SimpleUCAHandler: new SimpleUCAHandler()
-};
+// a handler that only accepts users over 21(ish)
+class AgeGateUCAHandler extends UCAHandler {
+  constructor() {
+    super('cvc:Identity:dateOfBirth')
+  }
+  async handleUCA(value, ucaState) {
+    if (value.year < new Date().getFullYear() - 21) {
+      ucaState.status = UCAStatus.ACCEPTED;
+    } else {
+      ucaState.status = UCAStatus.INVALID;
+      ucaState.error = new Error('user is underage')
+    }
+  }
+}
+
+module.exports = [
+  new AutoPassUCAHandler('cvc:Identity:name'),
+  new AutoPassUCAHandler('cvc:Identity:address'),
+  new AgeGateUCAHandler()
+];
 
 // NOOP handler
 // module.exports = (state,event) => state
