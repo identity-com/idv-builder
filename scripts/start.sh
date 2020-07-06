7cc71ae2240b521df1f0dc7864284681d78e1c28
Lucas Cunha <luccmir@gmail.com>
2020-07-06 16:55:46 -0300

    CIV-807 Improve scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/env bash
+#
+# Initialize the Idv with the Validation module plugin injected
+#
+# Usage:
+# ./scripts/start.sh
+
+DETACHED=true;
+DOCKER_COMPOSE_PARAMS='';
+
+if [ "$DETACHED" = true ]; then
+  DOCKER_COMPOSE_PARAMS='-d';
+fi
+
 DOCKER_REGISTRY=159876458955.dkr.ecr.us-east-1.amazonaws.com \
   CONFIG_OVERRIDE=$(scripts/configToString.sh) \
-  docker-compose up
+  docker-compose up $DOCKER_COMPOSE_PARAMS
--- test/scripts/startIdv.sh
+++ test/scripts/startIdv.sh
@@ -12,9 +12,7 @@ TEST_PATH=$(pwd);
 # initialize Idv
 cd ../;
 printf '\nStarting Idv...\n';
-DOCKER_REGISTRY=159876458955.dkr.ecr.us-east-1.amazonaws.com \
-  CONFIG_OVERRIDE=$(scripts/configToString.sh) \
-  docker-compose up -d
+./scripts/start.sh;
 cd ${TEST_PATH};
 
 # wait for application to be ready
