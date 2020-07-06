b77608822c79bf365a02e4f93f6b1cf62e1b9db0
Lucas Cunha <luccmir@gmail.com>
2020-07-06 14:27:54 -0300

    CIV-807 Implement initial tests

--- test/e2e/simpleUCAHandler.test.js
+++ test/e2e/simpleUCAHandler.test.js
@@ -5,22 +5,62 @@ const rp = require('request-promise-native');
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
+const validationModuleUrl = process.env.IDV_HOST || 'http://localhost:6060';
+
+const listPlans = async () => rp({
+  url: `${validationModuleUrl}/plans`,
+  method: 'GET',
+  json: true,
+  resolveWithFullResponse: true,
+});
+
+const createValidationProcess = async (credentialItemType, userId) => rp({
+  url: `${validationModuleUrl}/processes`,
+  method: 'POST',
+  body: { credentialItemType, userId },
+  json: true,
+  resolveWithFullResponse: true,
+});
+
+const patchUca = async (processId, userId, uca, value) => rp({
+  url: `${validationModuleUrl}/processes/${processId}/ucas/${uca}`,
+  method: 'PATCH',
+  body: { value },
+  headers: { user_id: userId },
+  json: true,
+  resolveWithFullResponse: true,
+});
+
 describe('Simple UCA handler E2E test', () => {
-  it('test', async () => {
-    expect(1+1).to.eq(2);
+  it('Should retrieve the plan for the credential-sample-v1 credential', async () => {
+    const response = await listPlans();
+    expect(response.statusCode).to.equal(200);
+    expect(response.body.data).to.include('credential-sample-v1');
   });
 
-  it('Create a new validation process', async () => {
-    const validationProcess = await rp({
-      url: 'http://localhost:6060/processes',
-      method: 'POST',
-      body: {
-        credentialItemType: 'credential-sample-v1',
-        userId: 'testUser'
-      },
-      json: true,
-      resolveWithFullResponse: true,
+  context('Success validation', () => {
+    let processId;
+    const userId = 'myUserId';
+
+    step('1. Create validation process', async () => {
+      const response = await createValidationProcess('credential-sample-v1', userId);
+      expect(response.statusCode).to.equal(201);
+      const { state, id } = response.body;
+      processId = id;
+      expect(state.credential).to.equal('credential-sample-v1');
+      expect(state.status).to.equal('IN_PROGRESS');
     });
-    console.log(validationProcess);
+
+    step('2. Submit the email UCA', async () => {
+      const nameValue = {
+        givenNames: 'Test',
+        familyNames: 'User',
+      };
+      const response = await patchUca(processId, userId, 'name', nameValue);
+      expect(response.statusCode).to.equal(202);
+      const { state } = response.body;
+      expect(state.ucas.name.status).to.equal('ACCEPTED');
+      expect(state.ucas.name.value).to.deep.equal(nameValue);
+    });
   });
 });
--- test/scripts/startIdv.sh
+++ test/scripts/startIdv.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+#
+# Initialize the Idv with the Validation module plugin injected.
+# Wait for the Idv to be ready.
+#
+# Usage:
+# ./scripts/startIdv.sh
 
+IDV_HEALTH_CHECK_URL='http://localhost:6060/health';
 TEST_PATH=$(pwd);
 
 # initialize Idv
@@ -11,11 +18,10 @@ DOCKER_REGISTRY=159876458955.dkr.ecr.us-east-1.amazonaws.com \
 cd ${TEST_PATH};
 
 # wait for application to be ready
-IDV_HEALTH_CHECK_URL='http://localhost:6060/health';
 printf '\nWaiting for Idv to be ready';
 until $(curl --output /dev/null --silent --head --fail ${IDV_HEALTH_CHECK_URL}); do
   printf '.';
   sleep 5;
 done
 
-printf '\nIdv is ready!\n'
+printf '\nIdv is ready!\n';
