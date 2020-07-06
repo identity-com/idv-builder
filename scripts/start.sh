212cb68b9f09b56251e92532f05df4b9913469ce
Lucas Cunha <luccmir@gmail.com>
2020-07-06 09:25:34 -0300

    CIV-807 setup docker-compose to read form the remote registry

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,3 @@
-CONFIG_OVERRIDE=$(scripts/configToString.sh) docker-compose up
+DOCKER_REGISTRY=159876458955.dkr.ecr.us-east-1.amazonaws.com \
+  CONFIG_OVERRIDE=$(scripts/configToString.sh) \
+  docker-compose up
