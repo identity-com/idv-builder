a5d64b8e0dba63309f663c0b0b00d8de4bdb43c6
Lucas Cunha <luccmir@gmail.com>
2020-07-09 13:53:51 -0300

    CIV-807 Add comments to the build scripts

--- scripts/pullIDVToolkitImages.sh
+++ scripts/pullIDVToolkitImages.sh
@@ -20,10 +20,12 @@ STAGE=${STAGE:-dev}
 
 STAGE=${STAGE} . scripts/identity-ecr-login.sh
 
+# The IDV-Toolkit images are downloaded from the Identity AWS container repository.
+# Production AWS account ID is 146055947386. Development AWS account ID is 159876458955.
 if [ "$STAGE" = 'prod' ]; then
-  DOCKER_REGISTRY='146055947386.dkr.ecr.us-east-1.amazonaws.com';
+  IDENTITY_AWS_ACCOUNT_ID='146055947386';
 else
-  DOCKER_REGISTRY='159876458955.dkr.ecr.us-east-1.amazonaws.com';
+  IDENTITY_AWS_ACCOUNT_ID='159876458955';
 fi;
 
-DOCKER_REGISTRY=${DOCKER_REGISTRY} docker-compose pull ${IMAGE}
+DOCKER_REGISTRY="${IDENTITY_AWS_ACCOUNT_ID}".dkr.ecr.us-east-1.amazonaws.com docker-compose pull ${IMAGE}
