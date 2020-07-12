751ea389c31b7170326e666751751102a85a6574
dankelleher <dankelleher@yahoo.com>
2020-07-12 09:37:14 +0200

    CIV-52 Initial changes for k8s configmaps upport

--- scripts/configToString.sh
+++
@@ -1,4 +0,0 @@
-# A temporary helper script to stringify the config file for the validation module
-# before configuration has been refactored
-
-echo $(cat ValidationModule/src/config/local-docker.json)
