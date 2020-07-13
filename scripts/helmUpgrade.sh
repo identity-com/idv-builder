ba5daf667375a1c36017693056fe649e814b4f88
dankelleher <dankelleher@yahoo.com>
2020-07-13 11:45:02 +0200

    CIV-52 Helm chart config changes

--- scripts/helmUpgrade.sh
+++ scripts/helmUpgrade.sh
@@ -1,7 +1,8 @@
 STAGE=${1:-sample}
 
 # Helm can only access files within its chart folder, so copy them here.
+rm -rf deploy/kubernetes/idv/config
 cp -r config deploy/kubernetes/idv/config
 
 cd deploy/kubernetes/idv || exit
-helm upgrade --install idv-${STAGE} . --namespace idv-${STAGE} -f values.yaml -f values.${STAGE}.yaml --debug --dry-run
+helm upgrade --install idv-${STAGE} . --namespace idv-${STAGE} -f values.yaml -f values.${STAGE}.yaml --debug
